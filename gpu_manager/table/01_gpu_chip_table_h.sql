20cff0d8f6c72da113c5ff8661bc1d2cefea336e
regalr <97627296+regalr@users.noreply.github.com>
2022-01-12 19:44:27 +0100

    Add files via upload

---
+++ gpu_manager/table/01_gpu_chip_table_h.sql
@@ -0,0 +1,16 @@
+CREATE TABLE gpu_chip_h(
+  gpu_chip_id                         NUMBER         NOT NULL
+ ,gpu_name                          VARCHAR2(10)         NOT NULL
+ ,foundry                          NUMBER         NOT NULL
+ ,directx                          NUMBER         NOT NULL
+ ,fp16                          NUMBER         NOT NULL
+ ,fp64                          NUMBER         NOT NULL
+ ,int32                          NUMBER         NOT NULL
+ ,pt_cores                          NUMBER        
+  ,mod_user                          varchar2(300)
+ ,created_on                        timestamp(6)
+ ,last_mod                          timestamp(6)
+ ,dml_flag	                        varchar2(1)
+ ,version	                          number
+) 
+TABLESPACE users;
